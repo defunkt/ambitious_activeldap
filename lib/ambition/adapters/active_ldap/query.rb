79fbd286a0eedcc6965892077bafd4df371a2727
Matthew King <automatthew@gmail.com>
2008-01-30 19:43:46 -0600

    removed to_hash as useless

--- lib/ambition/adapters/active_ldap/query.rb
+++ lib/ambition/adapters/active_ldap/query.rb
@@ -4,23 +4,18 @@ module Ambition
       class Query
 
         def kick
-          owner.find(:all, to_hash)
+          owner.find(:all, :filter => self.to_s)
         end
 
         def size
           raise "Not Implemented"
         end
 
-        def to_hash
-          hash = {}
-          hash[:filter] = to_s unless to_s.empty?
-          hash
-        end
-
         def to_s
           Array(clauses[:select]).join
           clauses[:select].first.to_s
         end
+        
       end
     end
   end
